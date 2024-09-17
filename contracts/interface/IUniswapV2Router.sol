36fc7d9c3e29bb56c4de68071f719ee9d3572843
Agbakwuru Oluchi <152165721+oluchicharity@users.noreply.github.com>
2024-09-17 16:43:14 +0100

    addAndRemoveLiquidityETH

---
+++ contracts/interface/IUniswapV2Router.sol
@@ -0,0 +1,74 @@
+    // SPDX-License-Identifier: UNLICENSED
+    pragma solidity ^0.8.9;
+    interface IUniswapV2Router {
+        function WETH() external view returns (address);
+        function swapExactTokensForTokens(
+            uint amountIn,
+            uint amountOutMin,
+            address[] calldata path,
+            address to,
+            uint deadline
+        ) external returns (uint[] memory amounts);
+        
+        function swapTokensForExactTokens(
+            uint amountOut,
+            uint amountInMax,
+            address[] calldata path,
+            address to,
+            uint deadline
+        ) external returns (uint[] memory amounts);
+
+        function swapTokensForExactETH(
+            uint amountOut, uint amountInMax,
+            address[] calldata path,
+            address to, uint deadline
+        ) external returns (uint[] memory amounts);
+
+        function addLiquidity(
+            address tokenA,
+            address tokenB,
+            uint amountADesired,
+            uint amountBDesired,
+            uint amountAMin,
+            uint amountBMin,
+            address to,
+            uint deadline
+        ) external returns (uint amountA, uint amountB, uint liquidity);
+
+        function addLiquidityETH(
+            address token,
+            uint amountTokenDesired,
+            uint amountTokenMin,
+            uint amountETHMin,
+            address to,
+            uint deadline
+            ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+
+        function removeLiquidity(
+            address tokenA,
+            address tokenB,
+            uint liquidity,
+            uint amountAMin,
+            uint amountBMin,
+            address to,
+            uint deadline
+            ) external returns (uint amountA, uint amountB);
+
+        function swapETHForExactTokens(
+            uint amountOut, 
+            address[] calldata path, 
+            address to, 
+            uint deadline)
+            external
+            payable
+            returns (uint[] memory amounts);
+
+        function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+        ) external returns (uint amountToken, uint amountETH);
+    }
